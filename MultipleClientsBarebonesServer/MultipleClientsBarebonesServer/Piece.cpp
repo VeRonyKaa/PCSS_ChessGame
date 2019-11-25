4eef7d734d42d20bcd94c13b87ecec9cd1e35c99
SweatyTryhardx10 <45422839+SweatyTryhardx10@users.noreply.github.com>
2019-11-25 01:24:59 +0100

    Adde the server for the chess game

---
+++ MultipleClientsBarebonesServer/MultipleClientsBarebonesServer/Piece.cpp
@@ -0,0 +1,41 @@
+#include "Piece.h"
+
+Piece::Piece() {};
+
+Piece::Piece(bool white, Type type1)	// Constructor
+{
+	isWhite = white;
+	type = type1;
+	moveCount = 0;
+}
+
+bool Piece::IsWhite()
+{
+	return isWhite;
+}
+
+Type Piece::GetType() {
+	return type;
+}
+
+void Piece::SetType(Type type2) {
+
+	type = type2;
+
+}
+
+void Piece::SetWhite(bool white) {
+	isWhite = white;
+}
+
+int Piece::GetMoveCount() {
+	return moveCount;
+}
+
+void Piece::SetMoveCount(int movecount) {
+	moveCount = movecount;
+}
+
+Piece::~Piece()	// Deconstructor
+{
+}
